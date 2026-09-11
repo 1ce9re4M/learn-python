f3fdf8e331bc75aae552342e236e547ff576353b
1ce9re4M <157962197+1ce9re4M@users.noreply.github.com>
2026-09-12 03:16:27 +0900

    Add files via upload

--- Chapter2/chapter2-2.py
+++ Chapter2/chapter2-2.py
@@ -24,4 +24,57 @@ numbers = [0, 10, 20, 30, 40, 50]
 chara = ['か', 'お', 'え']
 # print(min(chara))     #max,minは文字列にも適用可能
 
-#リストと演算子から
+#リストと演算子
+# print(numbers + ['a', 'b', 'c'])
+# print(numbers * 3)
+# print(['a'] * 10)
+x = [[0,1], ['a,b']]
+# print(x[0][0])
+y = x*3
+# print(y)
+x[0][0] = 100
+# print(y)
+
+#       a1 ＝＝　in　である。orが多いときはinを使えば見やすい
+a1 = 1
+# print(a1 == 1 or a1 == 3 or a1 == 7, a1 in [1, 3, 7])
+a1 = 5
+# print(a1 == 1 or a1 == 3 or a1 == 7, a1 in [1, 3, 7])
+
+#       indexはリストでも使える。findはリストで使うことはできない
+# print(numbers.index(20))
+# print(numbers.find(20)) #これはエラーになる
+
+numbers = ['b', 'a', 'c', '10', 'あ', 'd']
+# numbers.sort()
+# print(numbers)      #これはnumbersをそのまま入れ替えた(10, a, b, c, d, 'あ')
+
+x = sorted(numbers)
+# print(x)  #これはnumbersとxがそれぞれ存在(sorted関数)
+
+x.sort(reverse = True)
+# print(x)        #これは降順
+
+# print(sorted(numbers, reverse=True))   #これも降順
+
+ins = [[1,5], [5,2], [3,4], [4,1], [2,3]]
+ins.sort()
+# print(ins)      #多重リストもできちゃう
+
+x.append('20')  #リストに要素を入れる.append()
+x.append('30')
+x.append('40')
+
+x.extend(['100', '200'])    #リストにリストを追加.extend
+# print(x)
+
+numbers = [10, 20, 30, 40, 20]
+numbers.insert(1, 1000)
+# print(numbers)              #リストに要素を挿入する
+
+numbers.remove(30)
+# print(numbers)                #要素を削除（値を直接入れる）
+numbers.remove(20)
+# print(numbers)                #該当箇所が複数の場合最初を消す
+
+#       インデックス指定で削除
